13ed7b4fce7a11b8e8aba1b05d25e90851918180
naveen-bolle <245122748028@mvsrec.edu.in>
2025-03-25 13:31:09 +0530

    Add files via upload

---
+++ AppletDemo2.java
@@ -0,0 +1,15 @@
+import java.applet.*;
+import java.awt.*;
+
+/*<applet code="AppletDemo2.class" width="300" height="300"></applet>*/
+public class AppletDemo2 extends Applet
+{
+public void paint(Graphics g)
+{
+g.setColor(Color.RED);
+int x[]={200,300,300,200,100,100};
+int y[]={0,100,500,700,500,100};
+//g.fillPolygon(x,y,6);
+g.drawArc(100,120,130,140,150,160);
+}
+}
